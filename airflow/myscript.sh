25dd8a858a9ca9c7b79df01d46b695f2636ef62d
zng489 <45658785+zng489@users.noreply.github.com>
2024-03-19 09:01:18 -0300

    commit_18_03_2024

---
+++ airflow/myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a simple Bash script
+
+#echo "Hello, World!"
+#echo "Today is $(date)"
+
+#source C:/Users/PC/miniconda3/envs/dbt_big_query/Scripts
+cd "C:\Users\PC\Desktop\Git\dbt\dbt_big_query"
