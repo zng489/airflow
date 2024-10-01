aad850d0cb5129012949d51378254fb460dff62c
unknown <zhang489@hotmail.com>
2024-10-01 14:34:40 -0300

    limpando

--- airflow_scheduler_webserver/dags/azure.py
+++ airflow_scheduler_webserver/dags/azure.py
@@ -7,7 +7,24 @@ from airflow.operators.mysql_operator import MySqlOperator
 import pandas as pd
 import requests
 import json
+import os
+import shutil
+import logging
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from azure.storage.filedatalake import FileSystemClient
+from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables
+account_name = os.getenv('ACCOUNT_NAME')
+account_key = os.getenv('ACCOUNT_KEY')
+container_name = os.getenv('CONTAINER_NAME')
 
 default_args = {
     'owner': 'user',
@@ -19,20 +36,22 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
-dag = DAG('Helloworld', schedule_interval="@once", default_args=default_args)
+dag = DAG('Get_Azure_Data', schedule_interval="@once", default_args=default_args)
 
-def get_data(**kwargs):
-    url="https://jsonplaceholder.typicode.com/albums"
-    resp = requests.get(url)
-    if resp.status_code ==200:
-        res = resp.json()
-        return res
-    return -1
+def get_azure(**kwargs):
+    connect_str = f'DefaultEndpointsProtocol=https;AccountName=' + account_name + ';AccountKey=' + account_key + ';EndpointSuffix=core.windows.net'
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    container_client = blob_service_client.get_container_client(container_name)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_list = []
+    for blob_i in container_client.list_blobs():
+        print(blob_i)
+    return
 
 py = PythonOperator(
     task_id='py_opt',
     dag=dag,
-    python_callable=save_db,
+    python_callable=get_azure,
 )
 
 
@@ -40,4 +59,4 @@ t1 = BashOperator(
     task_id='task_1',
     bash_command='echo "Hello World from Task 1"',
     dag=dag)
-t1 >> py1 >> py
+t1 >> py
